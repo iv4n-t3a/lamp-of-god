7b3b503bfa77242a4be2d29cfcc80aa0e80fb910
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2024-11-19 13:27:51 +0300

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "gui.hpp"
 
+const double kFps = 30;
+const Vector<int> kWinSize = {360, 640};
+const std::string kWinName = "Lamp Of God";
+
 int main() {
   // TODO: implement
-  Gui gui(nullptr);
+  Gui gui(nullptr, kFps, kWinSize, kWinName);
   gui.Run();
 }
